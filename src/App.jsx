80f05f07aea925a2236813b1cf740c4520b38684
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2025-04-16 18:16:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       
+      
+      </div>
+      <h1 className='text-6xl font-bold text-amber-700'>Lasset hope page</h1>
+   
+    </>
+  )
+}
+
+export default App
