9b79c7878c0d0dea317a51a6609dd528d0477140
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2025-04-16 19:06:02 +0600

    added the template navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-       
+       testing
+       <Navbar></Navbar>
       
       </div>
       <h1 className='text-6xl font-bold text-amber-700'>Lasset hope page</h1>
